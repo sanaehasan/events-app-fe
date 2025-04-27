bd9690bb851b3cd7ce216c211468ba88411e1813
sanae <sanae.k1986@gmail.com>
2025-04-27 21:47:41 +0100

    fe prep

--- events-app-fe/src/App.tsx
+++ events-app-fe/src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Header/>
+      <NavBar/>
+       <Routes>
+        <Route path="/" element={<Home/>} />
+       </Routes>
     </>
   )
 }
---
+++ events-app-fe/src/components/NavBar.tsx
@@ -0,0 +1,5 @@
+function NavBar(){
+    return <p>navbar</p>
+}
+
+export default NavBar
