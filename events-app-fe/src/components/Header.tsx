0292ac10039146d86fc767c03402959af935bcb7
sanae <sanae.k1986@gmail.com>
2025-05-04 23:40:27 +0100

    fix links

--- events-app-fe/src/components/Header.tsx
+++ events-app-fe/src/components/Header.tsx
@@ -121,7 +121,7 @@ function handleLogout(){
                   About
                 </Link>
 
-                {(context!.user)?<Link to="#"
+                {(context!.user)?<Link to="/profile"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Profile
